343a3511e5bbdd6dd7960c13c112df8f264b8ea3
Pablo <pabloogarciatraan5@gmail.com>
2026-09-17 20:50:32 +0200

    tarea1 subida

---
+++ ut2-javascript/tareas/tarea1-adivinanumero.js
@@ -0,0 +1,31 @@
+const prompt = require('prompt-sync')();
+
+
+const numeroAdivinar = Math.floor(Math.random() * 11);
+let adivinado = false;
+let intentos = 0; // Usamos esta variable global para el bucle
+
+do {
+    let entrada = prompt("Adivina un número aleatorio del 0 al 10");
+    
+    
+    if (entrada === null || entrada.trim() === "") {
+        console.log('No se ha proporcionado ningún número');
+        continue; // Salta al final del bucle para pedir el número otra vez
+    }
+    
+    let intento = Number(entrada);
+    intentos++; 
+
+    if (intento === numeroAdivinar) {
+        console.log('¡Has adivinado el número!');
+        adivinado = true;
+    } else {
+        console.log('No has adivinado el número');
+    }
+
+} while (!adivinado && intentos < 5); 
+
+if (!adivinado) {
+    console.log(`Perdiste. El número era el ${numeroAdivinar}`);
+}
